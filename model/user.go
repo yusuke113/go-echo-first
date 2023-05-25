fd4819b285b85394002aafde98d52944554e3135
Yusuke Sato <yusuke.engineer@gmail.com>
2023-05-25 15:49:30 +0900

    fix:User構造体を修正

--- model/user.go
+++ model/user.go
@@ -4,15 +4,15 @@ import "time"
 
 type User struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email" gorm:"unique"`
+	Name      string    `json:"name" gorm:"not null"`
+	Email     string    `json:"email" gorm:"unique; not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type UserResponse struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name"`
+	Name      string    `json:"name" gorm:"not null"`
 	Email     string    `json:"email" gorm:"unique"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
